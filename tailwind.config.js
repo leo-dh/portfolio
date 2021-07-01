eface27e37a84f9fd5f5a6c51ac1d8a9eef369c1
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-07-01 13:41:19 +0800

    added and updated packages - framer-motion - tailwindcss - nextjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+      screen: {
+        tablet: "600px",
+        desktop: "1280px",
+        widescreen: "1920px",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        "fade-out": {
+          "0%": {
+            opacity: 1,
+          },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out",
+        "fade-out": "fade-out 0.3s ease-out",
       },
     },
   },

90d4caff37e773d4d3806c8e56302b59a05a7d1c
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-08-12 01:06:10 +0800

    added placeholder pages

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+const About = (): JSX.Element => {
+  return <div className="h-screen">Hello </div>;
+};
+
+export default About;
---
+++ pages/projects.tsx
@@ -0,0 +1,5 @@
+const Projects = (): JSX.Element => {
+  return <div className="h-screen">Hello</div>;
+};
+
+export default Projects;

cd0a0407b38ce3b02cf69755f74580a50204c288
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-08-11 16:47:38 +0800

    added link hover animation

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,10 +1,29 @@
 import { useState, HTMLProps, useEffect } from "react";
 import Link from "next/link";
-import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { AnimatePresence, AnimateSharedLayout, motion, Variants } from "framer-motion";
 import ProjectCard from "./ProjectCard";
 import ProjectModal from "./ProjectModal";
 import { ChevronRightIcon } from "./Icons";
 import { PROJECTS_DETAILS } from "@utils/PublicData";
+
+const svgVariants: Variants = {
+  rest: {
+    x: 0,
+    transition: {
+      duration: 1,
+      type: "tween",
+      ease: "easeIn",
+    },
+  },
+  hover: {
+    x: 5,
+    transition: {
+      duration: 0.4,
+      type: "tween",
+      ease: "easeOut",
+    },
+  },
+};
 
 const ProjectsSection = ({ className, ...props }: HTMLProps<HTMLElement>): JSX.Element => {
   const [selectedId, setSelectedId] = useState<null | number>(null);
@@ -15,10 +34,16 @@ const ProjectsSection = ({ className, ...props }: HTMLProps<HTMLElement>): JSX.E
     <section className={`flex flex-col py-12 ${className}`} {...props}>
       <div className="flex items-end ml-8 tablet:ml-16">
         <h1 className="text-4xl font-black uppercase">Projects </h1>
-        <Link href="/projects">
-          <a className="font-light tracking-wider ml-4">
-            more <ChevronRightIcon className="h-3 w-3 inline" />
-          </a>
+        <Link href="/projects" passHref>
+          <motion.a
+            className="font-light tracking-wider ml-4 hover:text-jungle-green-500 duration-300 ease-in-out"
+            whileHover="hover"
+          >
+            more
+            <motion.div variants={svgVariants} className="inline-block">
+              <ChevronRightIcon className="h-3 w-3 inline" />
+            </motion.div>
+          </motion.a>
         </Link>
       </div>
       <AnimateSharedLayout type="crossfade">

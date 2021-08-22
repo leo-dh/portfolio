53c69a556312a461df22e9989d50604edbc6e20e
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-08-22 16:13:00 +0800

    added temp about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,12 @@
 const About = (): JSX.Element => {
-  return <div className="h-screen">Hello </div>;
+  return (
+    <section className="flex flex-col justify-center min-h-screen px-8 py-12 tablet:p-16">
+      <h1 className="text-4xl font-bold uppercase tracking-wider font-futura text-jungle-green-500">
+        About Me
+      </h1>
+      <p className="text-5xl font-bold mt-16">WORK IN PROGRESS... </p>
+    </section>
+  );
 };
 
 export default About;

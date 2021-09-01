30f954c663558f9eec06f69a1f533f6d23e04f43
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-09-01 20:15:13 +0800

    added timeline section

--- pages/about.tsx
+++ pages/about.tsx
@@ -54,6 +54,20 @@ const About = ({ info, technicalSkills, timeline }: AboutProps): JSX.Element => 
           </div>
           <div className="mt-12">
             <h2 className="text-3xl font-bold uppercase font-futura tracking-wider">Timeline</h2>
+            <ul className="mt-8 space-y-4">
+              {timeline.map(({ start, end, description, institution }, index) => (
+                <li className="flex" key={index}>
+                  <div className="border-r-4 border-jungle-green-500 pr-4 flex flex-col justify-between text-sm">
+                    <p>{end}</p>
+                    <p className="text-shark-200">{start}</p>
+                  </div>
+                  <div className="ml-4">
+                    <h3 className="font-semibold text-lg">{institution}</h3>
+                    <p className="text-gray-400 mt-2 text-sm">{description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </section>

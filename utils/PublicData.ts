9fea0a4acdd8ce8a2060f0572773368f57fc7479
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-09-02 16:00:00 +0800

    chore: update gitignore and removed unnecessary data

--- utils/PublicData.ts
+++ utils/PublicData.ts
@@ -1,25 +1,2 @@
 export const EMAIL = "me@leodh.dev";
 export const GITHUB_PROFILE = "https://github.com/leo-dh";
-
-export const PROJECTS_DETAILS = [
-  {
-    title: "CityPop Landing Page",
-    tags: ["TypeScript", "Vue", "SCSS"],
-  },
-  {
-    title: "CityPop Landing Page",
-    tags: ["TypeScript", "Vue", "SCSS"],
-  },
-  {
-    title: "CityPop Landing Page",
-    tags: ["TypeScript", "Vue", "SCSS"],
-  },
-  {
-    title: "CityPop Landing Page",
-    tags: ["TypeScript", "Vue", "SCSS"],
-  },
-  {
-    title: "CityPop Landing Page",
-    tags: ["TypeScript", "Vue", "SCSS"],
-  },
-];
